cd3a3c9e2740fcb83c2c78837f7cda9d3d2c0132
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-05-11 23:50:03 +0700

    Update anu.sh

--- anu.sh
+++ anu.sh
@@ -1,4 +1,4 @@
 apt update 
 apt install keepalived -y
-apt-get install build-essential libssl-dev
+apt-get install build-essential libssl-dev -y
 wget http://www.keepalived.org/software/keepalived-1.2.19.tar.gz
